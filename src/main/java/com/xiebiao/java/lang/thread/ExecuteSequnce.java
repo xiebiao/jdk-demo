085afd12e81e7e4b04bb1def41de2b74e26953bb
bjxieb <xiebiao@jd.com>
2019-02-18 18:05:05 +0800

    test

---
+++ src/main/java/com/xiebiao/java/lang/thread/ExecuteSequnce.java
@@ -0,0 +1,34 @@
+package com.xiebiao.java.lang.thread;
+
+/**
+ * How to make two threads execute in sequence?
+ *
+ * @author bjxieb
+ * @date 2019-02-18
+ **/
+public class ExecuteSequnce {
+
+    static Thread A = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            System.out.println("A is run");
+        }
+    });
+    static Thread B = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            try {
+                A.join();
+                System.out.println("B is run");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    });
+
+    public static void main(String[] args) {
+
+        B.start();
+        A.start();
+    }
+}
---
+++ src/main/java/com/xiebiao/java/lang/thread/package-info.java
@@ -0,0 +1,4 @@
+package com.xiebiao.java.lang.thread;
+/**
+ * https://www.tutorialdocs.com/article/java-inter-thread-communication.html
+ */
